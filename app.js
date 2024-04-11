3175aaaa83114cf71cdc1cb31a456edc57307661
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-04-12 01:48:40 +0530

    Kaushak

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
+// const fs = require('fs');
 const bodyParser = require('body-parser');
-const https = require('https')
+//const SA = require('./superadmin/SA');
+//const TMS_logs = require('./tms_trigger_logs');
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/privkey.pem', 'utf8');
-const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.in/fullchain.pem', 'utf8');
-const credentials = { key: privateKey, cert: fullchain };
 
 const app = express();
 
@@ -27,9 +26,7 @@ app.get('/api/test', (req, res) => {
 });
 
 // Start the server
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(3000, () => {
-  console.log(`HTTPS server listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
