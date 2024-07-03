7b3c9732e0c5b9ff3195d5d25e1c6f59717f4941
Kaushal <kaushalpohekar1@gmail.com>
2024-07-03 13:10:26 +0530

    Kaushal

---
+++ app_test.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', router);
+
+app.get('/api/test', (req, res) => {
+  console.log('Received GET request to /api/test');
+  res.send('Response from Node.js server');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
