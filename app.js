1930cda2bdd795e6639bec06cb5c99981027e653
Kaushal <kaushalpohekar1@gmail.com>
2024-10-26 12:49:36 +0530

    Kaushal

--- app.js
+++ app.js
@@ -16,19 +16,42 @@ const port = 3000;
 
 const allowedOrigins = ['https://senso.senselive.in', 'http://localhost:4200'];
 
-app.use(cors());
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.json());
+
+// app.use('/api', router);
+// app.get('/api/test', (req, res) => {
+//   console.log('Received GET request to /api/example');
+//   res.send('Response from Node.js server');
+// });
+
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (!allowedOrigins.includes(origin)) {
+//     return res.status(403).sendFile(path.join(__dirname, 'public', 'access_denied.html'));
+//   }
+//   next();
+// });
+const allowedOrigins = ['https://elkem.senselive.in', 'http://localhost:4200'];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/api', router);
-app.get('/api/test', (req, res) => {
-  console.log('Received GET request to /api/example');
-  res.send('Response from Node.js server');
-});
-
+// Middleware to check origin
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (!allowedOrigins.includes(origin)) {
+    // Serve the access denied HTML page if the origin is not allowed
     return res.status(403).sendFile(path.join(__dirname, 'public', 'access_denied.html'));
   }
   next();
