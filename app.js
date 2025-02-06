ef7c1ee88b632d5c944961a99ea9533b41c126f7
Kaushal <kaushalpohekar1@gmail.com>
2025-02-06 13:18:11 +0530

    Kaushal

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const router = require('./routes');
-// const fs = require('fs');
-// const bodyParser = require('body-parser');
-// const https = require('https');
-
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/privkey.pem', 'utf8');
-// const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/fullchain.pem', 'utf8');
-// const credentials = { key: privateKey, cert: fullchain };
-
-// const app = express();
-
-// const port = 3000;
-
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use('/api', router);
-// app.get('/api/test', (req, res) => {
-//   console.log('Received GET request to /api/example');
-//   res.send('Response from Node.js server');
-// });
-
-// const httpsServer = https.createServer(credentials, app);
-
-// httpsServer.listen(3000, () => {
-//   console.log(`HTTPS server listening on port ${port}`);
-// });
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
@@ -35,7 +5,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const https = require('https');
 const path = require('path');
-//require('./dash/alert');
+require('./dash/alert');
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/privkey.pem', 'utf8');
 const fullchain = fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/fullchain.pem', 'utf8');
